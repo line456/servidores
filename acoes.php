c2c2dbea1ab10f8664bfb1636706fd9a03c7e3e4
line456 <aline.s.bezerra.aluno.senai.br>
2025-12-02 10:10:50 -0500

    servidor

--- acoes.php
+++ acoes.php
@@ -12,4 +12,16 @@ function selecionarTabela ($nomeDaTabela){
         echo "Tabela $nomeDaTabela não encontrada ";
     }
 }
+function selecionarTabelaPeloId ($nomeDaTabela,$id){
+    global $conexao;
+    $sql = "SELECT * FROM $nomeDaTabela WHERE id = $id"; //pegando a tabela 
+    $resultado = $conexao -> query($sql);
+
+    if ($resultado -> num_rows > 0 ){
+       return $resultado;
+    }
+    else{
+        echo "Tabela $nomeDaTabela não encontrada ";
+    }
+}
 ?>
