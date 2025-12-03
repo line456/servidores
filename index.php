f3c5c75e4b13e3a61dfd796651bf3476991eca60
line456 <aline.s.bezerra@aluno.senai.br>
2025-12-03 10:06:45 -0500

    parte final

--- index.php
+++ index.php
@@ -2,8 +2,48 @@
 require_once "acoes.php";
 
 $table_users = selecionarTabela('usuarios');
-
-foreach($table_users as $key => $value){
-    echo $value ['nome'] . "<br>";
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de usuarios</title>
+</head>
+<body>
+   <table>
+    <tr>
+        <th>Id</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Senha</th>
+        <th>Cargo</th>
+    </tr>>
+    <?php foreach ($table_users as $user): ?> 
+       <tr>
+         <td>
+           <?php echo $user ['id'] ?>
+
+        </td>
+        <td>
+           <?php echo $user ['nome'] ?>
+
+        </td>
+         <td>
+           <?php echo $user ['email'] ?>
+
+        </td>
+         <td>
+           <?php echo $user ['senha'] ?>
+
+        </td>
+         <td>
+           <?php echo $user ['cargo'] ?>
+
+        </td>
+       </tr>
+    <?php endforeach  ?>
+   </table>
+</body>
+</html>
